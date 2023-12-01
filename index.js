84a3c111752a75700e0a9854af99c24f1f7d37f2
thesalmankhxn <thesalmankhxn@gmail.com>
2023-12-01 17:18:01 +0530

    feature: add schema & resolvers for games review

---
+++ index.js
@@ -0,0 +1,45 @@
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+
+import db from "./_db.js";
+
+import { typeDefs } from "./schema.js";
+
+const resolvers = {
+  Query: {
+    games() {
+      return db.games;
+    },
+    game(parent, args) {
+      return db.games.find((game) => game.id === args.id);
+    },
+    authors() {
+      return db.authors;
+    },
+    author(parent, args) {
+      return db.authors.find((author) => author.id === args.id);
+    },
+    reviews() {
+      return db.reviews;
+    },
+    review(parent, args) {
+      return db.reviews.find((review) => review.id === args.id);
+    },
+  },
+  Game: {
+    reviews(parent) {
+      return db.reviews.filter((r) => r.game_id === parent.id);
+    },
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+const { url } = await startStandaloneServer(server, {
+  listen: { port: 4000 },
+});
+
+console.log("Server running at: ", 4000);
---
+++ schema.js
@@ -0,0 +1,30 @@
+export const typeDefs = `#graphql
+    type Game {
+        id: ID!
+        title: String!
+        platform: [String!]!
+        reviews: [Review!]
+    }
+    type Review {
+        id: ID!
+        rating: Int!
+        content: String!
+        game: Game!
+        author: Author!
+        game_id: ID
+    }
+    type Author {
+        id: ID!
+        name:  String!
+        verified: Boolean!
+        reviews: [Review!]
+    }
+    type Query {
+        reviews: [Review]
+        review(id: ID!): Review
+        games: [Game]
+        game(id: ID!): Game
+        authors: [Author]
+        author(id: ID!): Author
+    }
+`;
